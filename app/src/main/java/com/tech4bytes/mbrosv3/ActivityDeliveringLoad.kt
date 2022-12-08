47465a51e75856b33f822f7ddba7a343d7eeda8a
Prasun Mondal <prasun.mondal@oracle.com>
2022-12-08 17:03:59 +0530

    update

---
+++ app/src/main/java/com/tech4bytes/mbrosv3/ActivityDeliveringLoad.kt
@@ -0,0 +1,11 @@
+package com.tech4bytes.mbrosv3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityDeliveringLoad : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delivering_load)
+    }
+}

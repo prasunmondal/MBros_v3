83abcf20fbd0b6358737c6d7e25125aa58dbd7cb
Prasun Mondal <prasun.mondal@oracle.com>
2022-12-08 17:56:43 +0530

    Add ActivityDeliveringDeliver

--- app/src/main/java/com/tech4bytes/mbrosv3/ActivityDeliveringLoad.kt
+++ app/src/main/java/com/tech4bytes/mbrosv3/ActivityDeliveringLoad.kt
@@ -1,11 +1,18 @@
 package com.tech4bytes.mbrosv3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ActivityDeliveringLoad : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_delivering_load)
+    }
+
+    fun goToDeliveringDeliverPage(view: View) {
+        val switchActivityIntent = Intent(this, ActivityDeliveringDeliver::class.java)
+        startActivity(switchActivityIntent)
     }
 }

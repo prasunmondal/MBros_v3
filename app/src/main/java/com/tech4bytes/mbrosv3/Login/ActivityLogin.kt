0de36c9b989198bc21e676213f3a2505c63c6216
Prasun Mondal <prasun.mondal@oracle.com>
2022-12-09 16:58:44 +0530

    use device id to login

--- app/src/main/java/com/tech4bytes/mbrosv3/Login/ActivityLogin.kt
+++ app/src/main/java/com/tech4bytes/mbrosv3/Login/ActivityLogin.kt
@@ -3,8 +3,23 @@ package com.tech4bytes.mbrosv3.Login
 import androidx.appcompat.app.AppCompatActivity
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings.Secure
+import com.google.gson.reflect.TypeToken
+import com.prasunmondal.postjsontosheets.clients.get.Get
+import com.prasunmondal.postjsontosheets.clients.get.GetResponse
+import com.prasunmondal.postjsontosheets.clients.post.serializable.PostObject
 import com.tech4bytes.mbrosv3.Loading.ActivityDeliveringLoad
+import com.tech4bytes.mbrosv3.ProjectConfig
+import com.tech4bytes.mbrosv3.Utils.Date.DateUtils
+import com.tech4bytes.mbrosv3.Utils.Logs.LogMe.DeviceInfoUtils
+import com.tech4bytes.mbrosv3.Utils.ObjectUtils.ListUtils
+import java.time.Instant
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import java.util.Date
+import java.util.stream.Collectors
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -19,11 +34,25 @@ class ActivityLogin : AppCompatActivity() {
             Roles.ADMIN -> goToAdminRole()
             Roles.DELIVERY -> goToDeliveryRole()
             Roles.COLLECTOR -> goToCollectorRole()
+            else -> logUnIdentifiedDevice()
         }
     }
 
+    private fun logUnIdentifiedDevice() {
+        val time = DateUtils.getCurrentTimestamp()
+        val id = System.currentTimeMillis().toString()
+
+        val obj = RolesModel(id, time, getPhoneId(), Roles.UNIDENTIFIED.toString())
+        PostObject.builder()
+            .scriptId(ProjectConfig.dBServerScriptURL)
+            .sheetId(ProjectConfig.DB_SHEET_ID)
+            .tabName(Config.SHEET_TAB_NAME)
+            .dataObject(obj as Any)
+            .build().execute()
+    }
+
     private fun goToCollectorRole() {
-        TODO("Not yet implemented")
+
     }
 
     private fun goToDeliveryRole() {
@@ -35,7 +64,28 @@ class ActivityLogin : AppCompatActivity() {
         TODO("Not yet implemented")
     }
 
-    private fun getRole(): Roles {
-        return Roles.DELIVERY
+    private fun getPhoneId(): String {
+        return Secure.getString(applicationContext.contentResolver,
+            Secure.ANDROID_ID);
+    }
+
+    private fun getRole(): Roles? {
+        val result: GetResponse = Get.builder()
+            .scriptId(ProjectConfig.dBServerScriptURL)
+            .sheetId(ProjectConfig.DB_SHEET_ID)
+            .tabName(Config.SHEET_TAB_NAME)
+            .build().execute()
+
+        val deviceList = result.parseToObject<RolesModel>(result.getRawResponse(), object: TypeToken<ArrayList<RolesModel>?>() {}.type)
+
+        deviceList.sortBy { it.id }
+        deviceList.reverse()
+
+        deviceList.forEach {
+            if(getPhoneId() == it.id) {
+                return Roles.valueOf(it.role)
+            }
+        }
+        return null
     }
 }

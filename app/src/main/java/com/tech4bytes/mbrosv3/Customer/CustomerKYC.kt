e843c57d191c11cf2f5f1396682d5561c0330698
Prasun Mondal <prasun.mondal@oracle.com>
2022-12-10 16:12:05 +0530

    CustomerKYC

---
+++ app/src/main/java/com/tech4bytes/mbrosv3/Customer/CustomerKYC.kt
@@ -0,0 +1,50 @@
+package com.tech4bytes.mbrosv3.Customer
+
+import com.google.gson.reflect.TypeToken
+import com.prasunmondal.postjsontosheets.clients.get.Get
+import com.prasunmondal.postjsontosheets.clients.get.GetResponse
+import com.tech4bytes.extrack.centralCache.CentralCache
+import com.tech4bytes.mbrosv3.ProjectConfig
+import com.tech4bytes.mbrosv3.Utils.Contexts.AppContexts
+
+data class CustomerKYCModel(var date: String,
+                            var nameEng: String,
+                            var nameBeng: String,
+                            var phNo1: String,
+                            var phNo2: String,
+                            var address: String)
+
+class CustomerKYC {
+
+    fun getAllCustomers(useCache: Boolean = true): List<CustomerKYCModel> {
+        val cacheKey = "allCustomersList"
+        val cacheResults = CentralCache.get<ArrayList<CustomerKYCModel>>(AppContexts.get(), cacheKey, useCache)
+
+        return if (cacheResults != null) {
+            cacheResults
+        } else {
+            val resultFromServer = getCustomerListFromServer()
+            CentralCache.put(cacheKey, resultFromServer)
+            resultFromServer
+        }
+    }
+
+    fun getCustomerByEngName(engName: String): CustomerKYCModel? {
+        getAllCustomers().forEach {
+            if(it.nameEng == engName)
+                return it
+        }
+        return null
+    }
+
+    private fun getCustomerListFromServer(): List<CustomerKYCModel> {
+        val result: GetResponse = Get.builder()
+            .scriptId(ProjectConfig.dBServerScriptURL)
+            .sheetId(ProjectConfig.DB_SHEET_ID)
+            .tabName(Customer_Config.SHEET_TAB_NAME)
+            .build().execute()
+
+        return result.parseToObject(result.getRawResponse(),
+            object : TypeToken<ArrayList<CustomerKYCModel>?>() {}.type)
+    }
+}

d80940cf753ed5cebb672e572cd8e23070a098b9
Prasun Mondal <prasun.mondal@oracle.com>
2022-12-15 21:14:56 +0530

    use same data for loading

--- app/src/main/java/com/tech4bytes/mbrosv3/Loading/ActivityDeliveringLoad.kt
+++ app/src/main/java/com/tech4bytes/mbrosv3/Loading/ActivityDeliveringLoad.kt
@@ -5,9 +5,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.tech4bytes.mbrosv3.Delivering.ActivityDeliveringDeliver
+import com.tech4bytes.mbrosv3.GetOrders.OrdersTotalModel
 import com.tech4bytes.mbrosv3.R
 import com.tech4bytes.mbrosv3.Utils.Android.UIUtils
 import com.tech4bytes.mbrosv3.Utils.Contexts.AppContexts
@@ -26,17 +28,26 @@ class ActivityDeliveringLoad : AppCompatActivity() {
         AppContexts.set(this, this)
         addListeners()
 
+        showLoadOrderData()
         if(isLoadingComplete()) {
             goToDeliveringDeliverPage()
         }
     }
 
+    private fun showLoadOrderData() {
+        val loadOrderObject = OrdersTotalModel.get(true)
+        val loadOrderKg = loadOrderObject.requiredKg
+        val loadOrderPc = loadOrderObject.requiredPc
+        UIUtils.setUIElementValue(this, LoadingOrdersTotalModel.getUiElement(view, OrdersTotalModel::requiredPc), loadOrderPc.toString())
+        UIUtils.setUIElementValue(this, LoadingOrdersTotalModel.getUiElement(view, OrdersTotalModel::requiredKg), loadOrderKg.toString())
+    }
+
     private fun addListeners() {
-        (LoadingOrdersTotalModel.getUiElement(view, LoadingDataModel::actualPc) as EditText).addTextChangedListener {
+        (LoadingOrdersTotalModel.getUiElement(view, OrdersTotalModel::actualPc) as EditText).addTextChangedListener {
             updateAvgWeightText()
         }
 
-        (LoadingOrdersTotalModel.getUiElement(view, LoadingDataModel::actualKg) as EditText).addTextChangedListener {
+        (LoadingOrdersTotalModel.getUiElement(view, OrdersTotalModel::actualKg) as EditText).addTextChangedListener {
             updateAvgWeightText()
         }
     }
@@ -44,15 +55,15 @@ class ActivityDeliveringLoad : AppCompatActivity() {
     @SuppressLint("DefaultLocale")
     private fun updateAvgWeightText() {
         try {
-            LogMe.log((LoadingOrdersTotalModel.getUiElement(view, LoadingDataModel::actualKg) as EditText).text.toString())
-            LogMe.log((LoadingOrdersTotalModel.getUiElement(view, LoadingDataModel::actualPc) as EditText).text.toString())
-            val actualKg = UIUtils.getUIElementValue(LoadingOrdersTotalModel.getUiElement(view, LoadingDataModel::actualKg)).toDouble()
-            val actualPc = UIUtils.getUIElementValue(LoadingOrdersTotalModel.getUiElement(view, LoadingDataModel::actualPc)).toInt()
+            LogMe.log((LoadingOrdersTotalModel.getUiElement(view, OrdersTotalModel::actualKg) as EditText).text.toString())
+            LogMe.log((LoadingOrdersTotalModel.getUiElement(view, OrdersTotalModel::actualPc) as EditText).text.toString())
+            val actualKg = UIUtils.getUIElementValue(LoadingOrdersTotalModel.getUiElement(view, OrdersTotalModel::actualKg)).toDouble()
+            val actualPc = UIUtils.getUIElementValue(LoadingOrdersTotalModel.getUiElement(view, OrdersTotalModel::actualPc)).toInt()
             val avgWt = actualKg/actualPc
-            UIUtils.setUIElementValue(this, LoadingOrdersTotalModel.getUiElement(view, LoadingDataModel::avgWeight), format("%.3f", avgWt))
+            UIUtils.setUIElementValue(this, view.findViewById<TextView>(R.id.activity_delivering_load_actualAvgWt), format("%.3f", avgWt))
         } catch (e: NumberFormatException) {
             LogMe.log(e)
-            UIUtils.setUIElementValue(this, LoadingOrdersTotalModel.getUiElement(view, LoadingDataModel::avgWeight), "--.---")
+            UIUtils.setUIElementValue(this, view.findViewById<TextView>(R.id.activity_delivering_load_actualAvgWt), "--.---")
         }
 
     }

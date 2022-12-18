c0deafa603d3f137fce09eeb4494623532d8ef4e
Prasun Mondal <prasun.mondal@oracle.com>
2022-12-18 13:09:05 +0530

    Create activity

---
+++ app/src/main/java/com/tech4bytes/mbrosv3/CustomerOrders/DeliverOrders/deliverToACustomer/ActivityDeliveringDeliver.kt
@@ -0,0 +1,14 @@
+package com.tech4bytes.mbrosv3.CustomerOrders.DeliverOrders.deliverToACustomer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tech4bytes.mbrosv3.R
+import com.tech4bytes.mbrosv3.Utils.Contexts.AppContexts
+
+class ActivityDeliveringDeliver : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delivering_deliver)
+        AppContexts.set(this, this)
+    }
+}

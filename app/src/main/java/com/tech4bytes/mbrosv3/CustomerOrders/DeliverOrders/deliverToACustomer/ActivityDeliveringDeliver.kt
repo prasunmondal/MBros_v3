b18208fed310c3670ed6f48d2553f0ad008069c2
Prasun Mondal <prasun.mondal@oracle.com>
2022-12-18 20:42:08 +0530

    update delivering UI

--- app/src/main/java/com/tech4bytes/mbrosv3/CustomerOrders/DeliverOrders/deliverToACustomer/ActivityDeliveringDeliver.kt
+++ app/src/main/java/com/tech4bytes/mbrosv3/CustomerOrders/DeliverOrders/deliverToACustomer/ActivityDeliveringDeliver.kt
@@ -2,21 +2,147 @@ package com.tech4bytes.mbrosv3.CustomerOrders.DeliverOrders.deliverToACustomer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.widget.doOnTextChanged
+import com.tech4bytes.mbrosv3.CustomerOrders.GetOrders.GetCustomerOrders
 import com.tech4bytes.mbrosv3.R
+import com.tech4bytes.mbrosv3.Utils.Android.UIUtils
 import com.tech4bytes.mbrosv3.Utils.Contexts.AppContexts
 import com.tech4bytes.mbrosv3.Utils.Logs.LogMe.LogMe
 
 class ActivityDeliveringDeliver : AppCompatActivity() {
+
+    lateinit var mainView: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_delivering_deliver)
         AppContexts.set(this, this)
+        mainView = findViewById(R.id.activity_delivering_deliver_main)
 
-        val inputName = intent.extras!!.get("name")
+        val inputName = intent.extras!!.get("name") as String
         LogMe.log("Delivering to: $inputName")
+        initiallizeUI(inputName)
     }
 
-    fun getDataFromDeliveringCache() {
+    fun initiallizeUI(inputName: String) {
+        val record = getRecord(inputName)
 
+        // Get UI Elements
+        val nameElement = DeliverCustomerOrders.getUiElementFromDeliveringPage(mainView, DeliverCustomerOrders::name)
+        val orderedPcElement = DeliverCustomerOrders.getUiElementFromDeliveringPage(mainView, DeliverCustomerOrders::orderedPc)
+        val orderedKgElement = DeliverCustomerOrders.getUiElementFromDeliveringPage(mainView, DeliverCustomerOrders::orderedKg)
+        val deliveredPcElement = DeliverCustomerOrders.getUiElementFromDeliveringPage(mainView, DeliverCustomerOrders::deliveredPc)
+        val deliveredKgElement = DeliverCustomerOrders.getUiElementFromDeliveringPage(mainView, DeliverCustomerOrders::deliveredKg)
+        val todaysAmountElement = DeliverCustomerOrders.getUiElementFromDeliveringPage(mainView, DeliverCustomerOrders::todaysAmount)
+        val prevDueElement = DeliverCustomerOrders.getUiElementFromDeliveringPage(mainView, DeliverCustomerOrders::prevDue)
+        val totalDueElement = DeliverCustomerOrders.getUiElementFromDeliveringPage(mainView, DeliverCustomerOrders::totalDue)
+        val paidElement = DeliverCustomerOrders.getUiElementFromDeliveringPage(mainView, DeliverCustomerOrders::paid)
+        val balanceDueElement = DeliverCustomerOrders.getUiElementFromDeliveringPage(mainView, DeliverCustomerOrders::balanceDue)
+
+        // Set UI Values
+        UIUtils.setUIElementValue(this, nameElement, record.name)
+        UIUtils.setUIElementValue(this, orderedPcElement, record.orderedPc)
+        UIUtils.setUIElementValue(this, orderedKgElement, record.orderedKg)
+        UIUtils.setUIElementValue(this, deliveredPcElement, record.deliveredPc)
+        UIUtils.setUIElementValue(this, deliveredKgElement, record.deliveredKg)
+        UIUtils.setUIElementValue(this, todaysAmountElement, record.todaysAmount)
+        UIUtils.setUIElementValue(this, prevDueElement, record.prevDue)
+        UIUtils.setUIElementValue(this, totalDueElement, record.totalDue)
+        UIUtils.setUIElementValue(this, paidElement, record.paid)
+        UIUtils.setUIElementValue(this, balanceDueElement, record.balanceDue)
+
+        // Add Listeners
+        (deliveredKgElement as AppCompatEditText).doOnTextChanged { text, start, before, count ->
+            reCalculateNUpdateValues(record)
+        }
+        (paidElement as AppCompatEditText).doOnTextChanged { text, start, before, count -> reCalculateNUpdateValues(record) }
+    }
+
+    private fun reCalculateNUpdateValues(record: DeliverCustomerOrders) {
+        LogMe.log("Re-calculating...")
+        val deliveredKgElement = DeliverCustomerOrders.getUiElementFromDeliveringPage(mainView, DeliverCustomerOrders::deliveredKg)
+        val todaysAmountElement = DeliverCustomerOrders.getUiElementFromDeliveringPage(mainView, DeliverCustomerOrders::todaysAmount)
+        val totalDueElement = DeliverCustomerOrders.getUiElementFromDeliveringPage(mainView, DeliverCustomerOrders::totalDue)
+        val paidElement = DeliverCustomerOrders.getUiElementFromDeliveringPage(mainView, DeliverCustomerOrders::paid)
+        val balanceDueElement = DeliverCustomerOrders.getUiElementFromDeliveringPage(mainView, DeliverCustomerOrders::balanceDue)
+
+        val calcDeliveredKg = UIUtils.getUIElementValue(deliveredKgElement)
+        val paidStr = UIUtils.getUIElementValue(paidElement)
+
+        // Get Today's Amount
+        var calcTodaysAmount = 0.0
+        if(calcDeliveredKg.isNotEmpty() && calcDeliveredKg.toDouble() > 0) {
+            calcTodaysAmount = calcDeliveredKg.toDouble() * record.rate.toDouble()
+        }
+
+        // Get Total Amount
+        var calcPrevDue = 0.0
+        if(record.prevDue.isNotEmpty()) {
+            calcPrevDue = record.prevDue.toDouble()
+        }
+        val calcTotalAmount = calcPrevDue + calcTodaysAmount
+
+
+        // Get Balance Due Amount
+        var calcPaid = 0.0
+        if(paidStr.isNotEmpty()) {
+            calcPaid = paidStr.toDouble()
+        }
+        val calcBalanceDue = calcTotalAmount - calcPaid
+
+        UIUtils.setUIElementValue(this, todaysAmountElement, "$calcTodaysAmount")
+        UIUtils.setUIElementValue(this, totalDueElement, "$calcTotalAmount")
+        UIUtils.setUIElementValue(this, balanceDueElement, "$calcBalanceDue")
+    }
+
+
+    fun getRecord(inputName: String): DeliverCustomerOrders {
+        var deliveryObj: DeliverCustomerOrders? = getDataFromDeliveringRecords(inputName)
+        if(deliveryObj != null) {
+            return deliveryObj
+        }
+
+        val orderObj: GetCustomerOrders? = getDataFromOrderRecords(inputName)
+        if(orderObj != null) {
+            deliveryObj = DeliverCustomerOrders("",
+                "",
+                orderObj.name,
+                orderObj.estimatePc,
+                orderObj.estimateKg,
+                "",
+                "",
+                orderObj.rate,
+                "",
+                orderObj.due,
+                "",
+                "",
+                "",
+                "DELIVERING")
+
+            return deliveryObj
+        }
+        LogMe.log("We didn't find the record in delivering cache or orders placed")
+        return null!!
+    }
+
+    fun getDataFromDeliveringRecords(inputName: String): DeliverCustomerOrders? {
+        DeliverCustomerOrders.get().forEach {
+            if(it.name == inputName) {
+                return it
+            }
+        }
+        return null
+    }
+
+    fun getDataFromOrderRecords(inputName: String): GetCustomerOrders? {
+        GetCustomerOrders.get().forEach {
+            if(it.name == inputName) {
+                return it
+            }
+        }
+        return null
     }
 }
--- app/src/main/java/com/tech4bytes/mbrosv3/CustomerOrders/DeliverOrders/deliverToACustomer/DeliverCustomerOrders.kt
+++ app/src/main/java/com/tech4bytes/mbrosv3/CustomerOrders/DeliverOrders/deliverToACustomer/DeliverCustomerOrders.kt
@@ -1,5 +1,7 @@
 package com.tech4bytes.mbrosv3.CustomerOrders.DeliverOrders.deliverToACustomer
 
+import android.view.View
+import android.widget.TextView
 import com.google.gson.reflect.TypeToken
 import com.prasunmondal.postjsontosheets.clients.delete.Delete
 import com.prasunmondal.postjsontosheets.clients.get.Get
@@ -7,30 +9,34 @@ import com.prasunmondal.postjsontosheets.clients.get.GetResponse
 import com.prasunmondal.postjsontosheets.clients.post.serializable.PostObject
 import com.tech4bytes.extrack.centralCache.CentralCache
 import com.tech4bytes.mbrosv3.ProjectConfig
+import com.tech4bytes.mbrosv3.R
 import com.tech4bytes.mbrosv3.Utils.Contexts.AppContexts
+import kotlin.reflect.KMutableProperty1
 
 data class DeliverCustomerOrders(
     var id: String,
     var Timestamp: String,
     var name: String,
-    var pc: String,
-    var kg: String,
+    var orderedPc: String,
+    var orderedKg: String,
+    var deliveredPc: String,
+    var deliveredKg: String,
     var rate: String,
-    var total: String,
+    var todaysAmount: String,
     var prevDue: String,
     var totalDue: String,
     var paid: String,
-    var due: String,
+    var balanceDue: String,
     var deliveryStatus: String): java.io.Serializable {
     
     companion object {
-        fun <T> get(useCache: Boolean = true): List<T> {
-            val cacheResults = CentralCache.get<ArrayList<T>>(AppContexts.get(), DeliverOrdersConfig.SHEET_INDIVIDUAL_ORDERS_TAB_NAME, useCache)
+        fun get(useCache: Boolean = true): List<DeliverCustomerOrders> {
+            val cacheResults = CentralCache.get<ArrayList<DeliverCustomerOrders>>(AppContexts.get(), DeliverOrdersConfig.SHEET_INDIVIDUAL_ORDERS_TAB_NAME, useCache)
 
             return if (cacheResults != null) {
                 cacheResults
             } else {
-                val resultFromServer = getFromServer<T>()
+                val resultFromServer = getFromServer<DeliverCustomerOrders>()
                 CentralCache.put(DeliverOrdersConfig.SHEET_INDIVIDUAL_ORDERS_TAB_NAME, resultFromServer)
                 resultFromServer
             }
@@ -48,6 +54,22 @@ data class DeliverCustomerOrders(
                 .tabName(DeliverOrdersConfig.SHEET_INDIVIDUAL_ORDERS_TAB_NAME)
                 .build().execute()
             saveToLocal(listOf())
+        }
+
+        fun getUiElementFromDeliveringPage(view: View, attribute: KMutableProperty1<DeliverCustomerOrders, *>): View {
+            return when (attribute) {
+                DeliverCustomerOrders::name -> view.findViewById<TextView>(R.id.activity_delivering_deliver_name)
+                DeliverCustomerOrders::orderedPc -> view.findViewById<TextView>(R.id.activity_delivering_deliver_ordered_pc)
+                DeliverCustomerOrders::orderedKg -> view.findViewById<TextView>(R.id.activity_delivering_deliver_ordered_kg)
+                DeliverCustomerOrders::deliveredPc -> view.findViewById<TextView>(R.id.activity_delivering_deliver_delivering_pc)
+                DeliverCustomerOrders::deliveredKg -> view.findViewById<TextView>(R.id.activity_delivering_deliver_delivering_kg)
+                DeliverCustomerOrders::todaysAmount -> view.findViewById<TextView>(R.id.activity_delivering_deliver_todays_amount)
+                DeliverCustomerOrders::prevDue -> view.findViewById<TextView>(R.id.activity_delivering_deliver_prev_due)
+                DeliverCustomerOrders::totalDue -> view.findViewById<TextView>(R.id.activity_delivering_deliver_all_total)
+                DeliverCustomerOrders::paid -> view.findViewById<TextView>(R.id.activity_delivering_deliver_paid)
+                DeliverCustomerOrders::balanceDue -> view.findViewById<TextView>(R.id.activity_delivering_deliver_balance_due)
+                else -> null!!
+            }
         }
 
         private fun <T> saveObjectsToServer(objects: List<T>) {

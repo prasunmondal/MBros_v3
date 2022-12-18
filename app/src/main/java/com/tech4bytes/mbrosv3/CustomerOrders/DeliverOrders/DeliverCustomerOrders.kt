b1dbd7097af949756331bb8db11a5f3a60b6e2db
Prasun Mondal <prasun.mondal@oracle.com>
2022-12-18 12:17:45 +0530

    Create DeliverCustomerOrders.kt

---
+++ app/src/main/java/com/tech4bytes/mbrosv3/CustomerOrders/DeliverOrders/DeliverCustomerOrders.kt
@@ -0,0 +1,68 @@
+package com.tech4bytes.mbrosv3.CustomerOrders.DeliverOrders
+
+import com.google.gson.reflect.TypeToken
+import com.prasunmondal.postjsontosheets.clients.delete.Delete
+import com.prasunmondal.postjsontosheets.clients.get.Get
+import com.prasunmondal.postjsontosheets.clients.get.GetResponse
+import com.prasunmondal.postjsontosheets.clients.post.serializable.PostObject
+import com.tech4bytes.extrack.centralCache.CentralCache
+import com.tech4bytes.mbrosv3.ProjectConfig
+import com.tech4bytes.mbrosv3.Utils.Contexts.AppContexts
+
+data class DeliverCustomerOrders(var id: String, var Timestamp: String, var name: String, var pc: String, var kg: String, var rate: String, var total: String, var prevDue: String, var totalDue: String, var paid: String, var due: String) {
+    
+    companion object {
+        fun <T> get(useCache: Boolean = true): List<T> {
+            val cacheResults = CentralCache.get<ArrayList<T>>(AppContexts.get(), DeliverOrdersConfig.SHEET_INDIVIDUAL_ORDERS_TAB_NAME, useCache)
+
+            return if (cacheResults != null) {
+                cacheResults
+            } else {
+                val resultFromServer = getFromServer<T>()
+                CentralCache.put(DeliverOrdersConfig.SHEET_INDIVIDUAL_ORDERS_TAB_NAME, resultFromServer)
+                resultFromServer
+            }
+        }
+
+        fun save(objects: List<DeliverCustomerOrders>) {
+            saveObjectsToServer(objects)
+            saveToLocal(objects)
+        }
+
+        fun deleteAll() {
+            Delete.builder()
+                .scriptId(ProjectConfig.dBServerScriptURL)
+                .sheetId(ProjectConfig.DB_SHEET_ID)
+                .tabName(DeliverOrdersConfig.SHEET_INDIVIDUAL_ORDERS_TAB_NAME)
+                .build().execute()
+            saveToLocal(listOf())
+        }
+
+        private fun <T> saveObjectsToServer(objects: List<T>) {
+            objects.forEach {
+                PostObject.builder()
+                    .scriptId(ProjectConfig.dBServerScriptURL)
+                    .sheetId(ProjectConfig.DB_SHEET_ID)
+                    .tabName(DeliverOrdersConfig.SHEET_INDIVIDUAL_ORDERS_TAB_NAME)
+                    .dataObject(it as Any)
+                    .build().execute()
+            }
+        }
+
+        private fun saveToLocal(objects: List<DeliverCustomerOrders>) {
+            CentralCache.put(DeliverOrdersConfig.SHEET_INDIVIDUAL_ORDERS_TAB_NAME, objects)
+        }
+
+        private fun <T> getFromServer(): List<T> {
+            val result: GetResponse = Get.builder()
+                .scriptId(ProjectConfig.dBServerScriptURL)
+                .sheetId(ProjectConfig.DB_SHEET_ID)
+                .tabName(DeliverOrdersConfig.SHEET_INDIVIDUAL_ORDERS_TAB_NAME)
+                .build().execute()
+
+            return result.parseToObject(result.getRawResponse(),
+                object : TypeToken<ArrayList<T>?>() {}.type
+            )
+        }
+    }
+}

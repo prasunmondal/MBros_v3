da3510fac680168aac545d4ef627ce9b623efa37
Prasun Mondal <prasun.mondal@oracle.com>
2022-12-18 21:29:09 +0530

    skip loading page if done

--- app/src/main/java/com/tech4bytes/mbrosv3/Loading/LoadModel.kt
+++ app/src/main/java/com/tech4bytes/mbrosv3/Loading/LoadModel.kt
@@ -15,7 +15,17 @@ import com.tech4bytes.mbrosv3.Utils.Android.UIUtils
 import com.tech4bytes.mbrosv3.Utils.Contexts.AppContexts
 import kotlin.reflect.KMutableProperty1
 
-data class LoadModel(var id: String, var requiredKg: String, var requiredPc: String, var actualPc: String = "", var actualKg: String = ""): java.io.Serializable {
+data class LoadModel(var id: String,
+                     var requiredKg: String,
+                     var requiredPc: String,
+                     var actualPc: String = "",
+                     var actualKg: String = "",
+                     var loadingStatus: String = ""
+): java.io.Serializable {
+
+    fun isDone(): Boolean {
+        return this.loadingStatus == LoadConfig.string_tag__loadingStatus_completed
+    }
 
     companion object {
         fun get(useCache: Boolean = true): LoadModel {
@@ -32,6 +42,7 @@ data class LoadModel(var id: String, var requiredKg: String, var requiredPc: Str
         }
 
         fun save(obj: LoadModel) {
+            obj.loadingStatus = LoadConfig.string_tag__loadingStatus_completed
             saveObjectsToServer(obj)
             saveToLocal(obj)
         }

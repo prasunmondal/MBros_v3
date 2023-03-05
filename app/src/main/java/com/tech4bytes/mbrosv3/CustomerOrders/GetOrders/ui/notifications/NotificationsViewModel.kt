c238609b0ff53bbca164d36f276c1b117a494e5f
Prasun Mondal <prasun.mondal@oracle.com>
2023-03-05 12:58:33 +0530

    create generic bottom tabbed fragments

---
+++ app/src/main/java/com/tech4bytes/mbrosv3/CustomerOrders/GetOrders/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.tech4bytes.mbrosv3.CustomerOrders.GetOrders.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}

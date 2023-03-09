491786b1b998f220d5270655f1ca75e935792545
Prasun Mondal <prasun.mondal@oracle.com>
2023-03-09 11:38:20 +0530

    create OrdersMakeList

---
+++ app/src/main/java/com/tech4bytes/mbrosv3/CustomerOrders/GetOrders/OrdersMakeList.kt
@@ -0,0 +1,12 @@
+package com.tech4bytes.mbrosv3.CustomerOrders.GetOrders
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tech4bytes.mbrosv3.R
+
+class OrdersMakeList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_orders_make_list)
+    }
+}

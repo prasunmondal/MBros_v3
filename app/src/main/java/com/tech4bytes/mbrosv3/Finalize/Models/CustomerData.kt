05cdbe01abb9adb6137807c29dc6cba45e9ce5a2
Prasun Mondal <prasun.mondal@oracle.com>
2023-04-01 20:36:50 +0530

    fix

--- app/src/main/java/com/tech4bytes/mbrosv3/Finalize/Models/CustomerData.kt
+++ app/src/main/java/com/tech4bytes/mbrosv3/Finalize/Models/CustomerData.kt
@@ -65,7 +65,7 @@ class CustomerData: java.io.Serializable {
                 val profitByCustomer = totalProfit * NumberUtils.getDoubleOrZero(it.deliveredKg) / NumberUtils.getDoubleOrZero(SingleAttributedData.getRecords().actualLoadKg)
                 LogMe.log("ProfitPerCustomer: Name: ${it.name}: $totalProfit * ${NumberUtils.getDoubleOrZero(it.deliveredKg)} / ${NumberUtils.getDoubleOrZero(SingleAttributedData.getRecords().actualLoadKg)} = $profitByCustomer")
                 val profitPercentByCustomer = profitByCustomer / totalProfit * 100
-                val record = CustomerData(it.id, , it.name, it.deliveredPc, it.deliveredKg, it.rate, it.prevDue, it.todaysAmount, it.totalDue, it.paid, it.balanceDue, NumberUtils.roundOff2places(profitByCustomer).toString(), NumberUtils.roundOff2places(profitPercentByCustomer).toString())
+                val record = CustomerData(it.id, it.timestamp, it.name, it.deliveredPc, it.deliveredKg, it.rate, it.prevDue, it.todaysAmount, it.totalDue, it.paid, it.balanceDue, NumberUtils.roundOff2places(profitByCustomer).toString(), NumberUtils.roundOff2places(profitPercentByCustomer).toString())
                 addToFinalizeSheet(record)
             }
         }

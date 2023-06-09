005a024306e8ca32050ac76e3826af0b795af57c
Prasun Mondal <prasun.mondal@oracle.com>
2023-06-09 17:57:33 +0530

    Create CustomerDueData.kt

---
+++ app/src/main/java/com/tech4bytes/mbrosv3/Finalize/Models/CustomerDueData.kt
@@ -0,0 +1,26 @@
+package com.tech4bytes.mbrosv3.Finalize.Models
+
+import com.tech4bytes.mbrosv3.CustomerOrders.DeliverOrders.deliverToACustomer.DeliverToCustomerDataHandler
+import com.tech4bytes.mbrosv3.Utils.Numbers.NumberUtils
+
+class CustomerDueData {
+    companion object {
+
+        fun getBalance(shouldIncludePostDeliveryUpdates: Boolean = true): MutableMap<String, Int> {
+            val dueMap: MutableMap<String, Int> = mutableMapOf()
+            CustomerData.getAllLatestRecords().forEach {
+                dueMap[it.name] = NumberUtils.getIntOrZero(it.balanceDue)
+            }
+            if(shouldIncludePostDeliveryUpdates) {
+                DeliverToCustomerDataHandler.get().forEach {
+                    dueMap[it.name] = NumberUtils.getIntOrZero(it.balanceDue)
+                }
+            }
+            return dueMap
+        }
+
+        fun getBalance(name: String, shouldIncludePostDeliveryUpdates: Boolean = true): Int {
+            return getBalance(shouldIncludePostDeliveryUpdates)[name] ?: 0
+        }
+    }
+}

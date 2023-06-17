38a65f9a1513a7be14c7e35416161977787b8fb9
Prasun Mondal <prasun.mondal@oracle.com>
2023-06-17 23:30:40 +0530

    Update OneShotDelivery.kt

--- app/src/main/java/com/tech4bytes/mbrosv3/OneShot/Delivery/OneShotDelivery.kt
+++ app/src/main/java/com/tech4bytes/mbrosv3/OneShot/Delivery/OneShotDelivery.kt
@@ -443,7 +443,7 @@ class OneShotDelivery : AppCompatActivity() {
     private fun getTodaysSaleAmountForEntry(entry: View): Int {
         val kg = getKgForEntry(entry)
         val rate = getRateForEntry(entry)
-        val roundUpOffset = 0.00000000001
+        val roundUpOffset = 0.000001
         return (kg * rate + roundUpOffset).toInt()
     }
 

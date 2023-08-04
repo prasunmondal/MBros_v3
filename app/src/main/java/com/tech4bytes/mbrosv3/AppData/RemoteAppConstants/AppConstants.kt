24e9beaa8ef39c622b469d4457da7a092a6a2fc0
Prasun Mondal <prasun.mondal@oracle.com>
2023-08-04 18:27:42 +0530

    App Constants

---
+++ app/src/main/java/com/tech4bytes/mbrosv3/AppData/RemoteAppConstants/AppConstants.kt
@@ -0,0 +1,17 @@
+package com.tech4bytes.mbrosv3.AppData.RemoteAppConstants
+
+enum class AppConstants {
+    SMS_ORDER_SEQUENCE;
+
+    companion object {
+        fun get(constant: AppConstants, useCache: Boolean = true): String? {
+            val constantList = AppConstantsUtil.getAll(useCache)
+            constantList.forEach {
+                if(it.constantName == constant) {
+                    return it.constantValue
+                }
+            }
+            return null
+        }
+    }
+}

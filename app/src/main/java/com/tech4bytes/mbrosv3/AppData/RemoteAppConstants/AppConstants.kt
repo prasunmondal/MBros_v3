7aa311a03ef8def28dc89374c0d250f20c519564
Prasun Mondal <prasun.mondal@oracle.com>
2023-09-10 14:23:19 +0530

    fix

--- app/src/main/java/com/tech4bytes/mbrosv3/AppData/RemoteAppConstants/AppConstants.kt
+++ app/src/main/java/com/tech4bytes/mbrosv3/AppData/RemoteAppConstants/AppConstants.kt
@@ -8,9 +8,7 @@ enum class AppConstants {
     CAR_RATE_PER_KM,
     DRIVER_SALARY,
     SEND_DELIVERY_DETAILS_BY_SMS,
-    DELIVERY_BASE_RATE_DIFF,
-    WHATSAPP_NUMBER_MONTU,
-    SEND_LOAD_INFO_TEMPLATE_MONTU;
+    DELIVERY_BASE_RATE_DIFF;
 
     companion object {
         fun fetchAll(useCache: Boolean = true) {
@@ -21,7 +19,7 @@ enum class AppConstants {
             LogMe.log("Searching in AppConstants: $constant")
             val constantList = AppConstantsUtil.getAll(useCache)
             constantList.forEach {
-                if (it.constantName == constant) {
+                if (it.constantName == constant.name) {
                     LogMe.log("Searching in AppConstants: $constant: Successful. Value: ${it.constantValue}")
                     return it.constantValue
                 }
@@ -34,7 +32,7 @@ enum class AppConstants {
             LogMe.log("Searching in AppConstants: $constant")
             val constantList = AppConstantsUtil.getAll(useCache)
             constantList.forEach {
-                if (it.constantName != null && it.constantName.toString() == constant) {
+                if (it.constantName == constant) {
                     LogMe.log("Searching in AppConstants: $constant: Successful. Value: ${it.constantValue}")
                     return it.constantValue
                 }

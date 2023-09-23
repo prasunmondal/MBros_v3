757388ad2c201ff181544b31c9273321fef8b6e8
Prasun Mondal <prasun.mondal@oracle.com>
2023-09-23 22:00:29 +0530

    Update ActivityAdminDeliveryDashboard.kt

--- app/src/main/java/com/tech4bytes/mbrosv3/CustomerOrders/DeliverOrders/adminDashboard/ActivityAdminDeliveryDashboard.kt
+++ app/src/main/java/com/tech4bytes/mbrosv3/CustomerOrders/DeliverOrders/adminDashboard/ActivityAdminDeliveryDashboard.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -55,6 +56,9 @@ class ActivityAdminDeliveryDashboard : AppCompatActivity() {
         setContentView(R.layout.activity_admin_delivery_dashboard)
         supportActionBar!!.hide()
         AppContexts.set(this)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        
         AppUtils.logError()
         getPermissions()
 

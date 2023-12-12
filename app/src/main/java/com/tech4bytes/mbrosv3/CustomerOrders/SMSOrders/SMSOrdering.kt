7a969bdf2f41d0c521b03cd6076e01c6fd228386
prasunmondal <prasun.mondal02@gmail.com>
2023-12-12 20:40:14 +0530

    Update SMSOrdering.kt

--- app/src/main/java/com/tech4bytes/mbrosv3/CustomerOrders/SMSOrders/SMSOrdering.kt
+++ app/src/main/java/com/tech4bytes/mbrosv3/CustomerOrders/SMSOrders/SMSOrdering.kt
@@ -193,10 +193,11 @@ class SMSOrdering : AppCompatActivity() {
 
                 entry.findViewById<TextView>(R.id.smsorder_listEntry_date).text = orders[j].orderedKg.toString()
                 entry.findViewById<TextView>(R.id.smsorder_listEntry_number).text = orders[j].name
-                if(orders[j].name == CustomerKYC.getCustomerByEngName(orders[j].name)!!.customerAccount) {
-                    entry.findViewById<TextView>(R.id.smsorder_listEntry_amount).text = CustomerKYC.getCustomerByEngName(orders[j].name)!!.customerAccount    
+                if(orders[j].name != CustomerKYC.getCustomerByEngName(orders[j].name)!!.customerAccount) {
+                    entry.findViewById<TextView>(R.id.smsorder_listEntry_amount).text = CustomerKYC.getCustomerByEngName(orders[j].name)!!.customerAccount
+                } else {
+                    entry.findViewById<TextView>(R.id.smsorder_listEntry_amount).text = "$balance"
                 }
-                entry.findViewById<TextView>(R.id.smsorder_listEntry_amount).text = "$balance"
                 orderListContainer.addView(entry)
             }
         }

1c27b1a952ddde8fa7756df77e4d4be4aed99090
prasunmondal <prasun.mondal02@gmail.com>
2023-12-17 19:21:05 +0530

    Migrate StagedPayments to Tech4BytesSerializable

--- app/src/main/java/com/tech4bytes/mbrosv3/Payments/Staged/StagedPaymentUtils.kt
+++ app/src/main/java/com/tech4bytes/mbrosv3/Payments/Staged/StagedPaymentUtils.kt
@@ -1,95 +1,40 @@
 package com.tech4bytes.mbrosv3.Payments.Staged
 
 import com.google.gson.reflect.TypeToken
-import com.prasunmondal.postjsontosheets.clients.get.Get
-import com.prasunmondal.postjsontosheets.clients.get.GetResponse
-import com.prasunmondal.postjsontosheets.clients.post.serializable.PostObject
-import com.tech4bytes.extrack.centralCache.CentralCache
+import com.tech4bytes.mbrosv3.AppData.Tech4BytesSerializable
 import com.tech4bytes.mbrosv3.Payments.PaymentsType
 import com.tech4bytes.mbrosv3.ProjectConfig
-import com.tech4bytes.mbrosv3.Utils.Contexts.AppContexts
-import com.tech4bytes.mbrosv3.Utils.Logs.LogMe.LogMe
 import com.tech4bytes.mbrosv3.Utils.Numbers.NumberUtils
 
-class StagedPaymentUtils {
-    
-    companion object {
+object StagedPaymentUtils : Tech4BytesSerializable(
+    ProjectConfig.dBServerScriptURL,
+    ProjectConfig.get_db_sheet_id(),
+    "stagedPayments",
+    appendInServer = true,
+    appendInLocal = true,
+    cacheObjectType = object : TypeToken<ArrayList<StagedPaymentsModel>>() {}.type) {
 
-        fun getStagedPayments(name: String, useCache: Boolean = true): StagedPaymentsModel {
-            var list = getAllStagedPayments(useCache).filter { it.name == name }
-            if(list.isEmpty())
-                return StagedPaymentsModel(prevBalance = "",
-                    name = name,
-                    transactionType = PaymentsType.CREDIT,
-                    paidAmount = "0",
-                    newBalance = "",
-                    paymentMode = "",
-                    notes = "")
-
-            var sumPaid = 0
-            var allNotes = ""
-            list.forEach {
-                sumPaid += NumberUtils.getIntOrZero(it.paidAmount)
-                allNotes += "${it.name} paid Rs ${it.paidAmount} (${it.paymentMode}) recorded on ${it.datetime}."
-                if(it.notes.trim().isEmpty()) {
-                    allNotes += "[Note: ${it.notes}]."
-                }
-            }
-            return StagedPaymentsModel(
-                id = "",
-                datetime = "",
-                prevBalance = "",
-                name = name,
-                transactionType = PaymentsType.CREDIT,
-                paidAmount = sumPaid.toString(),
-                newBalance = "",
-                paymentMode = "",
-                notes = allNotes
-            )
-        }
-
-        fun getAllStagedPayments(useCache: Boolean = true): ArrayList<StagedPaymentsModel>{
-            LogMe.log("Getting delivery records")
-            val cacheResults = CentralCache.get<ArrayList<StagedPaymentsModel>>(AppContexts.get(), StagedPaymentsModel.tabname, useCache)
-            LogMe.log("Getting delivery records: Cache Hit: " + (cacheResults != null))
-            return if (cacheResults != null) {
-                cacheResults
-            } else {
-                val resultFromServer = getFromServer()
-                CentralCache.put(StagedPaymentsModel.tabname, resultFromServer)
-                resultFromServer
+    fun getStagedPayments(name: String, useCache: Boolean = true): StagedPaymentsModel {
+        val list = get<StagedPaymentsModel>(useCache).filter { it.name == name }
+        var sumPaid = 0
+        var allNotes = ""
+        list.forEach {
+            sumPaid += NumberUtils.getIntOrZero(it.paidAmount)
+            allNotes += "${it.name} paid Rs ${it.paidAmount} (${it.paymentMode}) recorded on ${it.datetime}."
+            if (it.notes.trim().isEmpty()) {
+                allNotes += "[Note: ${it.notes}]."
             }
         }
-
-        private fun getFromServer(): ArrayList<StagedPaymentsModel> {
-            val result: GetResponse = Get.builder()
-                .scriptId(ProjectConfig.dBServerScriptURL)
-                .sheetId(StagedPaymentsModel.sheet)
-                .tabName(StagedPaymentsModel.tabname)
-                .build().execute()
-
-            return result.parseToObject(
-                result.getRawResponse(),
-                object : TypeToken<ArrayList<StagedPaymentsModel>?>() {}.type
-            )
-        }
-
-        fun save(stagedObj: StagedPaymentsModel) {
-            saveToLocal(stagedObj)
-            saveToServer(stagedObj)
-        }
-
-        fun saveToLocal(stagedObj: StagedPaymentsModel) {
-            CentralCache.put(StagedPaymentsModel.tabname, stagedObj)
-        }
-
-        fun saveToServer(stagedObj: StagedPaymentsModel) {
-            PostObject.builder()
-                .scriptId(ProjectConfig.dBServerScriptURL)
-                .sheetId(StagedPaymentsModel.sheet)
-                .tabName(StagedPaymentsModel.tabname)
-                .dataObject(stagedObj as Any)
-                .build().execute()
-        }
+        return StagedPaymentsModel(
+            id = "",
+            datetime = "",
+            prevBalance = "",
+            name = name,
+            transactionType = PaymentsType.CREDIT,
+            paidAmount = sumPaid.toString(),
+            newBalance = "",
+            paymentMode = "",
+            notes = allNotes
+        )
     }
 }
--- app/src/main/java/com/tech4bytes/mbrosv3/Payments/Staged/StagedPaymentsModel.kt
+++ app/src/main/java/com/tech4bytes/mbrosv3/Payments/Staged/StagedPaymentsModel.kt
@@ -14,10 +14,4 @@ data class StagedPaymentsModel(
     var newBalance: String,
     var paymentMode: String,
     var notes: String
-): java.io.Serializable {
-
-    companion object {
-        var sheet = ProjectConfig.get_db_sheet_id()
-        var tabname = "stagedPayments"
-    }
-}
+): java.io.Serializable

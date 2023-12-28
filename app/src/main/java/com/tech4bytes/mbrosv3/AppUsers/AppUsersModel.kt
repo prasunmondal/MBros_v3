98df70bcaa104045a32d0f7132fa02f494540c48
prasunmondal <prasun.mondal02@gmail.com>
2023-12-28 11:21:28 +0530

    Migrate Roles on Tech4BytesSerialization

--- app/src/main/java/com/tech4bytes/mbrosv3/AppUsers/AppUsersModel.kt
+++ app/src/main/java/com/tech4bytes/mbrosv3/AppUsers/AppUsersModel.kt
@@ -2,10 +2,6 @@ package com.tech4bytes.mbrosv3.AppUsers
 
 data class AppUsersModel(val id: String, val timestamp: String, val device_id: String, var roles: String, var permissions: String) : java.io.Serializable {
 
-    companion object {
-
-    }
-
     override fun toString(): String {
         return "AppUsersModel(id='$id', timestamp='$timestamp', device_id='$device_id', roles='$roles', permissions='$permissions')"
     }
--- app/src/main/java/com/tech4bytes/mbrosv3/AppUsers/RolesUtils.kt
+++ app/src/main/java/com/tech4bytes/mbrosv3/AppUsers/RolesUtils.kt
@@ -1,62 +1,57 @@
 package com.tech4bytes.mbrosv3.AppUsers
 
 import android.provider.Settings
+import android.provider.Settings.Secure.ANDROID_ID
 import com.google.gson.reflect.TypeToken
-import com.prasunmondal.postjsontosheets.clients.get.Get
-import com.prasunmondal.postjsontosheets.clients.get.GetResponse
-import com.tech4bytes.extrack.centralCache.CentralCache
+import com.tech4bytes.mbrosv3.AppData.Tech4BytesSerializable
 import com.tech4bytes.mbrosv3.ProjectConfig
 import com.tech4bytes.mbrosv3.Utils.Contexts.AppContexts
 import com.tech4bytes.mbrosv3.Utils.Logs.LogMe.LogMe
 
-class RolesUtils {
+object RolesUtils : Tech4BytesSerializable<AppUsersModel>(
+    ProjectConfig.dBServerScriptURL,
+    ProjectConfig.get_db_sheet_id(),
+    "logins",
+    object : TypeToken<ArrayList<AppUsersModel>?>() {}.type,
+    appendInServer = true,
+    appendInLocal = true,
+    getEmptyListIfNoResultsFoundInServer = true
+) {
 
-    companion object {
+    fun getAppUser(useCache: Boolean = true, getEmptyListIfEmpty: Boolean = false, filterName: String = "default"): AppUsersModel? {
 
-        val loginRoleKey: String = "loginRoleKey"
-        fun getAppUser(useCache: Boolean = true): AppUsersModel? {
-            val cacheResults = CentralCache.get<AppUsersModel>(AppContexts.get(), loginRoleKey, useCache)
-
-            return if (cacheResults != null) {
-                cacheResults
-            } else {
-                val resultFromServer = getAppUsersDataFromServer()
-
-                CentralCache.put(loginRoleKey, resultFromServer)
-                resultFromServer
-            }
-        }
-
-        private fun getAppUsersDataFromServer(): AppUsersModel? {
-            // val waitDialog = ProgressDialog.show(AppContexts.get(), "Please Wait", "লোড হচ্ছে", true)
-            val result: GetResponse = Get.builder()
-                .scriptId(ProjectConfig.dBServerScriptURL)
-                .sheetId(ProjectConfig.get_db_sheet_id())
-                .tabName(Config.SHEET_TAB_NAME)
-                .build().execute()
-
-            val deviceList = result.parseToObject<AppUsersModel>(result.getRawResponse(), object : TypeToken<ArrayList<AppUsersModel>?>() {}.type)
-            deviceList.sortBy { it.id }
-            deviceList.reverse()
-
-            deviceList.forEach {
-                LogMe.log(it.toString())
-            }
-
-
-            deviceList.forEach {
-                if (getPhoneId() == it.device_id) {
-                    return it
-                }
-            }
+        val list = super.get(useCache, getEmptyListIfEmpty, filterName)
+        if(list.isEmpty())
             return null
+
+        return list[0]
+    }
+
+    override fun <T : Any> filterResults(list: ArrayList<T>): ArrayList<T> {
+        if(list.isEmpty())
+            return arrayListOf()
+
+        var list2 = list as ArrayList<AppUsersModel?>
+        list2.sortBy { it!!.id }
+        list2.reversed()
+
+        list2.forEach {
+            LogMe.log(it.toString())
         }
 
-        private fun getPhoneId(): String {
-            return Settings.Secure.getString(
-                AppContexts.get().contentResolver,
-                Settings.Secure.ANDROID_ID
-            );
+
+        list2.forEach {
+            if (getPhoneId() == it!!.device_id) {
+                return arrayListOf(it as T)
+            }
         }
+        return arrayListOf()
+    }
+
+    private fun getPhoneId(): String {
+        return Settings.Secure.getString(
+            AppContexts.get().contentResolver,
+            ANDROID_ID
+        )
     }
 }

04cf8bc1006c7c38b03155a7f552ee400874d793
prasunmondal <prasun.mondal02@gmail.com>
2024-03-13 10:09:52 +0530

    Create GetMultipleTabs

---
+++ app/src/main/java/com/tech4bytes/mbrosv3/Utils/DB/clients/getMultipleTabs/GetMultipleTabsFlow.kt
@@ -0,0 +1,31 @@
+package com.tech4bytes.mbrosv3.Utils.DB.clients.getMultipleTabs
+
+import java.util.function.Consumer
+import kotlin.reflect.KFunction
+
+interface GetMultipleTabsFlow {
+
+    interface ScriptIdBuilder {
+        fun scriptId(scriptId: String): SheetIdBuilder
+    }
+
+    interface SheetIdBuilder {
+        fun sheetId(sheetId: String): TabNameBuilder
+    }
+
+    interface TabNameBuilder {
+        fun tabName(tabName: String): SheetClassMapBuilder
+    }
+
+    interface SheetClassMapBuilder {
+        fun SheetClassMapBuilder(sheetClassMap: MutableMap<String, KFunction<Any>>): FinalRequestBuilder
+    }
+
+    interface FinalRequestBuilder {
+        // All optional parameters goes here
+        fun build(): GetMultipleTabs
+        fun postCompletion(onCompletion: Consumer<GetMultipleTabsResponse>?): FinalRequestBuilder
+    }
+
+    fun execute(): GetMultipleTabsResponse
+}

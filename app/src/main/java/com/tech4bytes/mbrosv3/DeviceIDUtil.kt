12c7709c1e239a032c2e32d20a454f6aa53e132d
prasunmondal <prasun.mondal02@gmail.com>
2024-08-07 14:19:43 +0530

    Update DeviceIDUtil.kt

--- app/src/main/java/com/tech4bytes/mbrosv3/DeviceIDUtil.kt
+++ app/src/main/java/com/tech4bytes/mbrosv3/DeviceIDUtil.kt
@@ -15,31 +15,28 @@ class DeviceIDUtil(private val context: Context) {
     private val sharedPreferences: SharedPreferences
         get() = context.getSharedPreferences(PREF_UNIQUE_ID, Context.MODE_PRIVATE)
 
-    private val uniqueID: String
-        get() {
-            // Check if the unique ID is already stored in SharedPreferences
-            val storedID = sharedPreferences.getString(PREF_UNIQUE_ID, null)
-            if (storedID != null) {
-                return storedID
-            }
-
-            // Generate a new unique ID
-            val androidId = Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
-            val newID: String = if (DEFAULT_ANDROID_ID == androidId) {
-                // Generate a random UUID if the default value is detected
-                UUID.randomUUID().toString()
-            } else {
-                // Generate a UUID based on the Android ID
-                UUID.nameUUIDFromBytes(androidId.toByteArray()).toString()
-            }
-
-            // Store the unique ID in SharedPreferences
-            sharedPreferences.edit().putString(PREF_UNIQUE_ID, newID).apply()
-            return newID
-        }
+//    private val uniqueID: String
 
     fun getUniqueID(): String {
-        return uniqueID
+        // Check if the unique ID is already stored in SharedPreferences
+        val storedID = sharedPreferences.getString(PREF_UNIQUE_ID, null)
+        if (storedID != null) {
+            return storedID
+        }
+
+        // Generate a new unique ID
+        val androidId = Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+        val newID: String = if (DEFAULT_ANDROID_ID == androidId) {
+            // Generate a random UUID if the default value is detected
+            UUID.randomUUID().toString()
+        } else {
+            // Generate a UUID based on the Android ID
+            UUID.nameUUIDFromBytes(androidId.toByteArray()).toString()
+        }
+
+        // Store the unique ID in SharedPreferences
+        sharedPreferences.edit().putString(PREF_UNIQUE_ID, newID).apply()
+        return newID
     }
 }
 

ff4e2803570cda6c8b56d5e5884ad4e4e6d6b774
prasunmondal <prasun.mondal02@gmail.com>
2024-09-07 00:13:01 +0530

    Update SMSOrderModel.kt

--- app/src/main/java/com/tech4bytes/mbrosv3/CustomerOrders/SMSOrders/SMSOrderModel.kt
+++ app/src/main/java/com/tech4bytes/mbrosv3/CustomerOrders/SMSOrders/SMSOrderModel.kt
@@ -4,6 +4,7 @@ import com.prasunmondal.dev.libs.contexts.AppContexts
 import com.prasunmondal.dev.libs.gsheet.ContextWrapper
 import com.prasunmondal.dev.libs.gsheet.clients.GSheetSerialized
 import com.tech4bytes.mbrosv3.ProjectConfig
+import java.io.Serializable
 
 data class SMSOrderModel(
     var id: String,
@@ -11,7 +12,7 @@ data class SMSOrderModel(
     var orderedKg: Int,
     var calculatedPc: Double,
     var orderedPc: Int
-)
+): Serializable
 object SMSOrderModelUtil: GSheetSerialized<SMSOrderModel> (
     context = ContextWrapper(AppContexts.get()),
     sheetId = ProjectConfig.get_db_sheet_id(),
